335fa84e1ddb6c2d58d5721161b4a1d86e45e44e
Marcin <jerzakm@gmail.com>
2020-05-18 14:19:48 +0200

    webgl test up

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import App from './App.svelte';
-import './theme/style.scss'
+import './theme/style.scss';
 import "@simonwep/pickr/dist/themes/nano.min.css";
+import {
+	runWebGLDemo
+} from './webgl/run';
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
+// const app = new App({
+// 	target: document.body,
+// 	props: {
+// 		name: 'world'
+// 	}
+// });
 
-export default app;
+// console.log('ok')
+
+// export default app;
+
+runWebGLDemo();
